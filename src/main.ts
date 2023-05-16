1f19c85cc9ca5ad3bc52ffc32f5a2b6b5d5bc654
Kevin Cefalu <kevin.cefalu@outlook.com>
2023-05-16 10:44:50 -0500

    started introducing bitburner code into repo

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
-import ms from 'ms';
-import lunchtime from './lunchtime.js';
-import millisecondsUntil from './millisecondsUntil.js';
+// import ms from 'ms';
+// import lunchtime from './managers/lunchtime.js';
+// import millisecondsUntil from './common/millisecondsUntil.js';
 
-export default function howLongUntilLunch(hours: number = 12, minutes: number = 30): string {
-  const millisecondsUntilLunchTime = millisecondsUntil(lunchtime(hours, minutes));
-  return ms(millisecondsUntilLunchTime, { long: true });
+// export default function howLongUntilLunch(hours: number = 12, minutes: number = 30): string {
+//   const millisecondsUntilLunchTime = millisecondsUntil(lunchtime(hours, minutes));
+//   return ms(millisecondsUntilLunchTime, { long: true });
+// }
+
+
+import { NS } from '@ns';
+
+export async function main(ns: NS) {
+  ns.tprint('Hello World!');
 }
